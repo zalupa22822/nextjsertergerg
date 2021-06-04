a54f3c0a8123b64aefd22b3f6190595ba965ca47
zalupa22822 <84581584+zalupa22822@users.noreply.github.com>
2021-06-04 20:26:39 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
+<meta name="facebook-domain-verification" content="6uh1fwax6b4c6dtjxmh42qf6xtotd4" />
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
